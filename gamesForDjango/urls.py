ad384034c9fbbc813cace0c4bd0826038d19178d
Papa Perry <calcal56@gmail.com>
2026-06-07 15:18:23 -0500

    refer to notes 6_7_2026

--- gamesForDjango/urls.py
+++ gamesForDjango/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    
+    path('', include('menu.urls')),
+    path('game1example/', include('game1example.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
